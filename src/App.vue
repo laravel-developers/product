2d958d757c7ec6fc48e0487c1145d15aff5d61fb
javohir <javohir@gmail.com>
2021-08-11 22:20:29 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <products :msg="msg" />
+    <h3>{{ name }}</h3>
+  </div>
+</template>
+
+<script>
+import Products from './components/products.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: 'Zohid',
+      name: 'Product project'
+    }
+  },
+  components: {
+    Products
+  },
+  created() {
+  },
+  mounted() {
+  },
+  computed: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/products.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="">
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Nomi</th>
+          <th>Narxi</th>
+          <th>Yaratilgan sana</th>
+          <th>Amal qilish muddati (oy)</th>
+        </tr>
+      </thead>
+      <tbody>
+        <template v-if="products.length">
+          <tr v-for="product in products" :key="product.id">
+            <td>{{ product.id }}</td>
+            <td>{{ product.name }}</td>
+            <td>{{ product.price }}</td>
+            <td>{{ product.created_date }}</td>
+            <td>{{ product.term }}</td>
+          </tr>
+        </template>
+        <template v-else>
+          <tr><td colspan="5">Malumot yo'q</td></tr>
+        </template>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      products: []
+    }
+  },
+  props: {
+    msg: String
+  },
+  created() {
+    axios.get('http://product.local/products').then((res) => {
+      this.products = res.data
+    })
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
