3c60a23bfdae10bf748163ed0f474b0a6474c94e
javohir <javohir@gmail.com>
2021-08-13 22:10:42 +0500

    update

--- src/components/products.vue
+++ src/components/products.vue
@@ -21,21 +21,22 @@
         <template v-if="products.length">
           <tr v-for="product in products" :key="product.id">
             <td>{{ product.id }}</td>
-            <td>{{ product.name }}</td>
+            <td><a href="#">{{ product.name }}</a></td>
             <td>{{ product.price }}</td>
             <td>{{ product.created_date }}</td>
             <td>{{ product.term }}</td>
             <td>
               <button class="btn btn-danger" @click="destroy(product.id)">O'chirish</button>
+              <a href=""><button class="btn btn-info" @click="update(product.id)">Tahrirlash</button></a>
             </td>
           </tr>
         </template>
         <template v-else>
-          <tr><td colspan="5">Malumot yo'q</td></tr>
+          <tr><td colspan="5">Ma`lumot yo'q</td></tr>
         </template>
       </tbody>
     </table>
-      <h3>Product qo'shish</h3>
+    <h3>Product qo'shish</h3>
     <form class="form container-fluid px-5" action="#">
       <div class="row">
         <div class="col-sm-4">
@@ -65,6 +66,31 @@
       </div>
       <button class="float-right" @click="save()">Saqlash</button>
     </form>
+
+<!--    modal-->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+      Launch demo modal
+    </button>
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -84,6 +110,12 @@ export default {
         price: '',
         created_date: '',
         term: ''
+      },
+      formEdit: {
+        name: '',
+        price: '',
+        created_date: '',
+        term: ''
       }
     }
   },
@@ -94,6 +126,14 @@ export default {
     this.getItems()
   },
   methods: {
+    update(id){
+      axios.post('http://product.local/products/' + id, this.form).then(() => {
+        this.getItems()
+      }).finally(() => {
+        this.isLoading = false
+        this.clearForm()
+      })
+    },
     destroy(id) {
       if(confirm('Haqiqatan ham o\'chirmoqchimisiz')) {
         axios.delete('http://product.local/products/' + id).then(() => {
@@ -110,6 +150,14 @@ export default {
     },
     save() {
       axios.post('http://product.local/products', this.form).then(() => {
+        this.getItems()
+      }).finally(() => {
+        this.isLoading = false
+        this.clearForm()
+      })
+    },
+    saveEdit() {
+      axios.post('http://product.local/products', this.formEdit).then(() => {
         this.getItems()
       }).finally(() => {
         this.isLoading = false
